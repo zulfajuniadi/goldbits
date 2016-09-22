b3f24c6107d0a331124ffc7e5331fb6631e5904d
Zulfa Juniadi bin Zulkifli <zulfajuniadi@gmail.com>
2016-09-23 05:47:08 +0800

    Add Former service provider

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Thujohn\Twitter\TwitterServiceProvider::class,
+        Former\FormerServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -227,6 +228,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Twitter' => Thujohn\Twitter\Facades\Twitter::class,
+        'Former' => Former\Facades\Former::class,
 
     ],
 
