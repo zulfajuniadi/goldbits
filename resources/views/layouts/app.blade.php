f3d7dbed9230483cace36a2eb0e4c3110b0945de
Zulfa Juniadi bin Zulkifli <zulfajuniadi@gmail.com>
2016-09-23 08:51:39 +0800

    Add products link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,8 +16,8 @@
     <!-- Scripts -->
     <script>
         window.Laravel = <?php echo json_encode([
-            'csrfToken' => csrf_token(),
-        ]); ?>
+    'csrfToken' => csrf_token(),
+]); ?>
     </script>
 </head>
 <body>
@@ -49,11 +49,12 @@
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
                     @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
+                        {{-- <li><a href="{{ url('/login') }}">Login</a></li>
+                        <li><a href="{{ url('/register') }}">Register</a></li> --}}
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                <a href="{{ url('/products') }}">Products</a>
                                 {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,8 +68,9 @@
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
-                    <a href="{{ url('/login') }}">Login</a>
-                    <a href="{{ url('/register') }}">Register</a>
+                    <a href="{{ url('/products') }}">Products</a>
+                    {{-- <a href="{{ url('/login') }}">Login</a>
+                    <a href="{{ url('/register') }}">Register</a> --}}
                 </div>
             @endif
 
