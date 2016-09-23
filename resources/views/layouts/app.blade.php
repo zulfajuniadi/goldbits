6e6a31d813f47a8ab8b57ce0a8a43e76edbec177
Zulfa Juniadi bin Zulkifli <zulfajuniadi@gmail.com>
2016-09-23 12:44:08 +0800

    Add fontawesome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,13 +11,14 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
     <link href="/css/app.css" rel="stylesheet">
 
     <!-- Scripts -->
     <script>
-        window.Laravel = {{json_encode([
+        window.Laravel = {!! json_encode([
             'csrfToken' => csrf_token(),
-        ])}}
+        ]) !!};
     </script>
 </head>
 <body>
